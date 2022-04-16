e9e8f389de0f1c5fe96274786ff7f5f36bc62368
Andre Peixoto <peixoto.amp@gmail.com>
2022-04-16 00:17:51 -0300

    Fix audio and background paths

--- index.js
+++ index.js
@@ -4,43 +4,43 @@ const playAudio = (audioPath) => {
 };
 
 const playOne = () => {
-  playAudio('../media/01.mp3');
+  playAudio('../julia-english-box/media/01.mp3');
 };
 
 const playTwo = () => {
-  playAudio('../media/02.mp3');
+  playAudio('../julia-english-box/media/02.mp3');
 };
 
 const playThree = () => {
-  playAudio('../media/03.mp3');
+  playAudio('../julia-english-box/media/03.mp3');
 };
 
 const playFour = () => {
-  playAudio('../media/04.mp3');
+  playAudio('../julia-english-box/media/04.mp3');
 };
 
 const playFive = () => {
-  playAudio('../media/05.mp3');
+  playAudio('../julia-english-box/media/05.mp3');
 };
 
 const playSix = () => {
-  playAudio('../media/06.mp3');
+  playAudio('../julia-english-box/media/06.mp3');
 };
 
 const playSeven = () => {
-  playAudio('../media/07.mp3');
+  playAudio('../julia-english-box/media/07.mp3');
 };
 
 const playEight = () => {
-  playAudio('../media/08.mp3');
+  playAudio('../julia-english-box/media/08.mp3');
 };
 
 const playNine = () => {
-  playAudio('../media/09.mp3');
+  playAudio('../julia-english-box/media/09.mp3');
 };
 
 const playTen = () => {
-  playAudio('../media/10.mp3');
+  playAudio('../julia-english-box/media/10.mp3');
 };
 
 const animateButton = function (e) {
